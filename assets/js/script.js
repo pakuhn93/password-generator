3b256004332c866b773db2cab925327bd868a3fb
pakuhn <pakuhn93@gmail.com>
2023-05-24 22:40:58 -0700

    password can be successfully generated and viewed in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+var password;
 var generateBtn = document.querySelector("#generate");
 var passMinLength = 8;
 var passMaxLength = 128;
@@ -6,6 +7,19 @@ var passMaxLength = 128;
 //array holding char types user may/may not want in their password
 var steps = [null, "special characters", "numbers", "upper case letters", "lower case letters"];
 var step = 0; //counter for which step we're on, used with steps array
+
+
+var chars = {
+  //*****NOT INCLUDING BACKSLASH FOR NOW*****
+  charSpecial: ["`", "~", "1", "@", "#", "$", "%", "^", "&", "*", 
+  "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", "\"",
+   "\'", ";", ":", ",", "<", ".", ">", "?", "/"], //NOT including " " spacebar, ran into weird issues when it was on the end of passwords
+  charNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  charUpper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+  "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  charLower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+}
 
 //the user's password criterea, initialized with default values
 var passwordCriterea = {
@@ -65,30 +79,57 @@ function generateCharacterTypes(){
   } //END pseudo-recursion
 }
 
-function generatePassword(){
+function createPassword(passLength, includeSpecial, includeNumber, includeUpper, includeLower){
+  var passChars = [];
+  var tempPass = "";
+
+  if(includeSpecial){ passChars = passChars.concat(chars.charSpecial) }
+  console.log(passChars);
+  if(includeNumber){ passChars = passChars.concat(chars.charNumber) }
+  console.log(passChars);
+  if(includeUpper){ passChars = passChars.concat(chars.charUpper) }
+  console.log(passChars);
+  if(includeLower){ passChars = passChars.concat(chars.charLower) }
+  console.log(passChars);
+  
+
+  for(var i = 0; i < passLength; i++){
+    tempPass = tempPass + passChars[Math.floor(Math.random() * passChars.length)];
+    console.log(tempPass);
+  }
+}
+
+function generatePassword(){ 
   //Prompt: length = 8 to 128 characters
-  //Prompt: include special characters y/n
-  //Prompt: include lower and uppercase y/n
-  //Prompt: include numbers y/n
   step = 0;
   passwordCriterea.passLength = generatePasswordLength();
   console.log("length = " + passwordCriterea.passLength);
 
+  //Prompt: include special characters y/n
   step = 1;
   passwordCriterea.includeSpecial = generateCharacterTypes();
   console.log("include special chars = " + passwordCriterea.includeSpecial);
 
+  //Prompt: include numbers y/n
   step = 2;
   passwordCriterea.includeNumber = generateCharacterTypes();
   console.log("include numbers = " + passwordCriterea.includeNumber);
 
+  //Prompt: include upper-case y/n
   step = 3;
   passwordCriterea.includeUpper = generateCharacterTypes();
   console.log("include upper-case = " + passwordCriterea.includeUpper);
 
+  //Prompt: include lower-case y/n
   step = 4;
   passwordCriterea.includeLower = generateCharacterTypes();
   console.log("include lower-case = " + passwordCriterea.includeLower);
+
+  //randomly creates the password based on received user input
+  createPassword(passwordCriterea.passLength, passwordCriterea.includeSpecial, 
+    passwordCriterea.includeNumber, passwordCriterea.includeUpper, 
+    passwordCriterea.includeLower);
+
 }
 
 generatePassword();
